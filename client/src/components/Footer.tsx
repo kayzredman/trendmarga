57584995ffddabc96a3c3199ac89fe5fd96bf18b
kayzredman <kayzredman@gmail.com>
2026-03-14 20:07:33 +0000

    update favicon to qodeio and smal patch on the footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
           </p>
         </Link>
         <p className="text-sm text-gray-400">© 2026 TrendMarga.</p>
-        <p className="text-sm text-gray-400">All rights reserved.</p>
+        <p className="text-sm text-gray-400">Powered by QodeIO. All rights reserved.</p>
       </div>
       <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
         <p className="text-sm text-amber-50">Links</p>
